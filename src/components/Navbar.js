0d85501262a1f023702c1a3ce948b332f9482f72
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-07-18 22:27:20 -0500

    nav bar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,82 @@
-// src/components/NavBar.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "reactstrap";
-import "./css/Navbar.css"; // Import the custom CSS file
+import "./css/Navbar.css";
+import stripesSVG from "../assets/navStripes.svg";
 
 const NavBar = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  const handleNavLinkClick = () => {
+    // Scrolls to the top of the page when a nav link is clicked
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Navbar
-      color="light"
-      light
       expand="md"
+      id="navbar"
+      className={isSticky ? "sticky" : ""}
     >
       <Container className="navbar-container">
         <Link
           to="/"
           className="navbar-brand"
-        >
-          Your Website Logo
-        </Link>
+        ></Link>
+
         <Nav className="navbar-nav">
           <Link
             to="/"
             className="nav-link"
             id="home-link"
+            onClick={handleNavLinkClick}
           >
-            {" "}
-            {/* Add ID for the Home link */}
             Home
+            <img
+              src={stripesSVG}
+              alt="Stripes"
+              className="stripes-svg"
+            />
           </Link>
+
           <Link
             to="/portfolio"
             className="nav-link"
             id="portfolio-link"
+            onClick={handleNavLinkClick}
           >
-            {" "}
-            {/* Add ID for the Portfolio link */}
             Portfolio
+            <img
+              src={stripesSVG}
+              alt="Stripes"
+              className="stripes-svg"
+            />
           </Link>
+
           <Link
             to="/contact"
             className="nav-link"
             id="contact-link"
+            onClick={handleNavLinkClick}
           >
-            {" "}
-            {/* Add ID for the Contact link */}
             Contact
+            <img
+              src={stripesSVG}
+              alt="Stripes"
+              className="stripes-svg"
+            />
           </Link>
         </Nav>
       </Container>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./css/home.css";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      {/* Content for testing purposes */}
+      <div className="tall-content">
+        <h1>End of Page</h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
