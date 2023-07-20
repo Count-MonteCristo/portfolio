ae448885a9afb073e9999c02bbedf54149c3f11d
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-07-20 18:21:21 -0500

    hero section content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,51 @@
 import React from "react";
 import "./css/home.css";
+import Typewriter from "typewriter-effect";
+import stripesSVG from "../assets/heroStripes.svg";
 
 const Home = () => {
   return (
     <>
-      {/* Content for testing purposes */}
-      <div className="tall-content">
-        <h1>End of Page</h1>
+      <div className="hero">
+        <div className="hero-text">
+          <h2 className="hero-subtitle">Hello, my name is Luis.</h2>
+          <h1 className="hero-heading">
+            <Typewriter
+              options={{
+                autoStart: true, // Automatically starts typing on mount
+                loop: true, // Loops the typewriter animation
+              }}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("I am a Web Designer")
+                  .pauseFor(1500)
+                  .deleteChars(12)
+                  .typeString("Full-Stack Developer")
+                  .pauseFor(1500)
+                  .deleteChars(21)
+                  .typeString("n SEO Editor")
+                  .pauseFor(1500)
+                  .start();
+              }}
+            />
+          </h1>
+          <p className="hero-description">
+            A one-stop shop for all your website needs.
+          </p>
+          <a
+            href="/contact"
+            className="hero-button"
+          >
+            Contact Me
+          </a>
+        </div>
+        <div className="hero-graphic">{/* Add graphic here */}</div>
       </div>
+      <img
+        src={stripesSVG}
+        alt="Stripes"
+        className="heroStripes-svg"
+      />
     </>
   );
 };
