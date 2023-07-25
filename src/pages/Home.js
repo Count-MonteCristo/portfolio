cfeafb2f8bbc9cfbdeaee3a1183a4ce97826da3a
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-07-24 21:10:00 -0500

    Adds services section and makes modifications to other sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,42 +3,45 @@ import "./css/home.css";
 import Typewriter from "typewriter-effect";
 import stripesSVG from "../assets/heroStripes.svg";
 import companyLogos from "../assets/companies.png";
+import TabbedInterface from "../components/TabbedInterface";
 
 const Home = () => {
   return (
     <>
       {/* Hero section */}
       <div className="hero">
-        <div className="hero-text">
-          <h2 className="hero-subtitle">Hello, my name is Luis.</h2>
-          <h1 className="hero-heading">
-            <Typewriter
-              options={{
-                autoStart: true, // Automatically starts typing on mount
-                loop: true, // Loops the typewriter animation
-              }}
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString("I am a Software Engineer")
-                  .pauseFor(1500)
-                  .deleteChars(18)
-                  .typeString("n SEO Editor")
-                  .pauseFor(1500)
-                  .start();
-              }}
-            />
-          </h1>
-          <p className="hero-description">
-            A one-stop shop for all your website needs.
-          </p>
-          <a
-            href="/contact"
-            className="hero-button"
-          >
-            Contact Me
-          </a>
+        <div className="container">
+          <div className="hero-text">
+            <h2 className="hero-subtitle">Hi, my name is Luis!</h2>
+            <h1 className="hero-heading">
+              <Typewriter
+                options={{
+                  autoStart: true, // Automatically starts typing on mount
+                  loop: true, // Loops the typewriter animation
+                }}
+                onInit={(typewriter) => {
+                  typewriter
+                    .typeString("I am a Software Engineer")
+                    .pauseFor(1500)
+                    .deleteChars(18)
+                    .typeString("n SEO Editor")
+                    .pauseFor(1500)
+                    .start();
+                }}
+              />
+            </h1>
+            <p className="hero-description">
+              An all-in-one solution for your website needs.
+            </p>
+            <a
+              href="/contact"
+              className="hero-button"
+            >
+              Contact Me
+            </a>
+          </div>
+          <div className="hero-graphic">{/* Add graphic here */}</div>
         </div>
-        <div className="hero-graphic">{/* Add graphic here */}</div>
       </div>
       <img
         src={stripesSVG}
@@ -58,6 +61,23 @@ const Home = () => {
                 className="company-logos"
               />
             </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Services section */}
+      <div className="services-section">
+        <div className="container">
+          <h2 className="servicesSection-title">What I do</h2>
+          <p className="servicesSection-p">
+            I'm a passionate Software Engineer and SEO Editor dedicated to
+            crafting exceptional online experiences. With a fusion of creativity
+            and technical expertise, I offer a range of services that empower
+            businesses to thrive in the digital realm. Here's what I can do for
+            you:
+          </p>
+          <div className="tabs">
+            <TabbedInterface />
           </div>
         </div>
       </div>
