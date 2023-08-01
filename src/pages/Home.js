60a8bdb5d53cae2d85b87afe1421dfaf33331818
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-07-31 22:12:38 -0500

    Home page update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,32 @@ import h2StripesSVG from "../assets/h2Stripes.svg";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { HiCode } from "react-icons/hi";
 import { MdSearch } from "react-icons/md";
+import { BiMailSend } from "react-icons/bi";
+import { BiLogoLinkedin } from "react-icons/bi";
+import { BsGithub } from "react-icons/bs";
 import ParkerDeweyHero from "../assets/ParkerDeweyHero.jpg";
 import CleanOriginHero from "../assets/CleanOriginHero.jpg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
+  const testimonials = [
+    {
+      id: 1,
+      name: "Eric R.",
+      company: "Texas A&M University-San Antonio",
+      testimonial:
+        "Luis would be an exceptional addition to any team ... Luis was always ready to meet any challenge that was presented to him and exceeded expectations. Our entire team was impressed by his ability to manage various projects and meet deadlines. I highly recommend Luis and know he will add value to your team.",
+    },
+    {
+      id: 2,
+      name: "Gus Z.",
+      company: "Parker Dewey",
+      testimonial:
+        "Luis is a dedicated, communicative, self-motivated, and courteous professional. Working with him was a pleasure, and I have no doubt he'd be an asset to any organization.",
+    },
+  ];
+
   const handleClick = () => {
     alert("Button Clicked!");
   };
@@ -102,7 +124,10 @@ const Home = () => {
           <div className="services-section-cards">
             <div className="services-section-card-design">
               <div className="services-section-content">
-                <MdOutlineDesignServices size={80} />
+                <MdOutlineDesignServices
+                  size={80}
+                  color={"#b7353c"}
+                />
                 <h3 className="services-section-content-heading">Design</h3>
                 <p className="services-section-content-text">
                   I create captivating digital experiences through intuitive
@@ -113,7 +138,10 @@ const Home = () => {
             </div>
             <div className="services-section-card-development">
               <div className="services-section-content">
-                <HiCode size={80} />
+                <HiCode
+                  size={80}
+                  color={"#D06A42"}
+                />
                 <h3 className="services-section-content-heading">
                   Development
                 </h3>
@@ -125,7 +153,10 @@ const Home = () => {
             </div>
             <div className="services-section-card-seo">
               <div className="services-section-content">
-                <MdSearch size={80} />
+                <MdSearch
+                  size={80}
+                  color={"#D0AD6C"}
+                />
                 <h3 className="services-section-content-heading">SEO</h3>
                 <p className="services-section-content-text">
                   I craft compelling content that will help your brand soar to
@@ -169,19 +200,31 @@ const Home = () => {
                   DEVELOPMENT • SEO
                 </div>
               </div>
-              <img
-                src={ParkerDeweyHero}
-                alt="Parker Dewey Website"
-                className="project-image"
-              />
+              <a
+                href="https://www.parkerdewey.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={ParkerDeweyHero}
+                  alt="Parker Dewey Website"
+                  className="project-image"
+                />
+              </a>
             </div>
 
             <div className="work-section-content-2">
-              <img
-                src={CleanOriginHero}
-                alt="Parker Dewey Website"
-                className="project-image"
-              />
+              <a
+                href="https://www.cleanorigin.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={CleanOriginHero}
+                  alt="Parker Dewey Website"
+                  className="project-image"
+                />
+              </a>
               <div className="work-section-content-2-card">
                 <div className="work-section-content-2-card-title">
                   Increased Online Visibility
@@ -267,6 +310,26 @@ const Home = () => {
             lasting impact. But don't just take my word for it. See what some of
             my amazing clients have to say
           </p>
+
+          {/* Testimonial carousel */}
+          <Carousel
+            showArrows={true}
+            showThumbs={false}
+            showStatus={false}
+            infiniteLoop={true}
+          >
+            {testimonials.map((testimonial) => (
+              <div
+                key={testimonial.id}
+                className="testimonial-item"
+              >
+                <p className="testimonial-text">{testimonial.testimonial}</p>
+
+                <p className="testimonial-name">{testimonial.name}</p>
+                <p className="testimonial-company">{testimonial.company}</p>
+              </div>
+            ))}
+          </Carousel>
         </div>
 
         {/* Contact section */}
@@ -275,14 +338,43 @@ const Home = () => {
           className="contact-section"
         >
           <div className="contact-section-heading">
-            <h2>Let’s Make it Happen</h2>
+            <h2>Let's Make it Happen</h2>
           </div>
-          <p className="content-section-text">
+          <p className="contact-section-text">
             If you're ready to take your online presence to the next level, I'm
             here to make it happen. Whether you need a compelling website, a
             powerful web application, or an SEO strategy that drives growth,
             I've got you covered.
           </p>
+
+          <div className="social-buttons">
+            <a href="mailto:luis0425navarro@gmail.com">
+              <button className="social-button">
+                <BiMailSend size={30} />
+                <div className="social-label">Mail</div>
+              </button>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/luisnavarrobarrera/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="social-button">
+                <BiLogoLinkedin size={30} />
+                <div className="social-label">LinkedIn</div>
+              </button>
+            </a>
+            <a
+              href="https://github.com/Count-MonteCristo/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="social-button">
+                <BsGithub size={30} />
+                <div className="social-label">GitHub</div>
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </>
