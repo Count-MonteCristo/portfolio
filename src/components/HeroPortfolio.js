26c8d2bc2a96467238ea9c669705b61e3bec8b90
Count-MonteCristo <79714613+Count-MonteCristo@users.noreply.github.com>
2023-08-02 14:04:38 -0500

    Adds portfolio page hero

--- src/components/HeroPortfolio.js
+++ src/components/HeroPortfolio.js
@@ -1,15 +1,54 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./css/heroPortfolio.css";
 import stripesSVG from "../assets/heroStripes.svg";
+import h2StripesSVG from "../assets/h2Stripes.svg";
 
 const HeroPortfolio = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <div
         id="home"
         className="hero"
       >
-        <div className="container"></div>
+        <div className="container">
+          <div className="portfolio-heading">Portfolio</div>
+          <div className="portfolio-description">
+            <div className="portfolio-text">
+              See how I have helped businesses transform their digital presence
+              and achieve their goals.
+            </div>
+            <div className="portfolio-counter">
+              <div className="successful-projects">
+                <div className="number">28</div>
+                <div className="label">Successful projects</div>
+              </div>
+              <img
+                src={h2StripesSVG}
+                alt="Stripes"
+                className="h2Stripes-svg"
+              />
+              <div className="experience">
+                <div className="number">1</div>
+                <div className="label">Year of experience</div>
+              </div>
+            </div>
+          </div>
+          <div className="hero-cta-button">
+            <button
+              onClick={handleClick}
+              className="cta-button-hero"
+            >
+              Let's work together
+            </button>
+          </div>
+        </div>
       </div>
       <img
         src={stripesSVG}
